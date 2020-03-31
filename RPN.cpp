0a70a39181535f043f99552e7d4f3273ca98b498
rajiv-williams <59885073+rajiv-williams@users.noreply.github.com>
2020-03-31 09:22:12 -0400

    Update RPN.cpp

--- RPN.cpp
+++ RPN.cpp
@@ -15,42 +15,81 @@ int main()
     stack<string> st;   // stack definition
 
     cin >> in;
-
+    st.push(in);
+    
+    int value1;
+    int value2;
+    int value3;    
+    int stackSize;
+    
     while(in != "q")
     {
-        if(is_str_digit(in)){
-            // add your code here
-        }
-        else if(is_str_operator(in))
-        {
-            // add your code here 
-
-            switch(in[0])
+        try{
+            if(is_str_digit(in)){
+                st.push(in);
+            }
+            else if(is_str_operator(in))
             {
-                case '+':
-                    value3 = value2 + value1;
-                    break;
+                stackSize = st.size();
+                try{
+                    if((stackSize - 1) < 2){//Stack already has 1 element by default which is the operator somehow
+                        //throw st.size();
+                        throw stackSize;
+                    }
+                }
+                catch(int e){
+                    cout << "ERROR: Stack does not have at least 2 operands but has " << e - 1 << " element(s) in the stack when operator is given!" << endl;
+                    exit(1);
+                }
+                
+                value1 = stoi(st.top());
+                st.pop();
+                value2 = stoi(st.top()); 
+                st.pop();
+                
+                switch(in[0])
+                {
+                    case '+':
+                        value3 = value2 + value1;
+                        break;
 
-                case '-':
-                    value3 = value2 - value1;
-                    break;
+                    case '-':
+                        value3 = value2 - value1;
+                        break;
 
-                case '*':
-                    value3 = value2 * value1;
-                    break;
+                    case '*':
+                        value3 = value2 * value1;
+                        break;
 
-                case '/':
-                    value3 = value2 / value1;
-                    break;
+                    case '/':
+                        value3 = value2 / value1;
+                        break;
+
+                }
+                st.push(to_string(value3));
 
             }
-
-            // add your code here
-
+            else{
+                throw in;
+            }
         }
+        catch(string e){
+            cout << "ERROR: " << '"' << e << '"' << " is not an integer nor an operator!" << endl;
+            exit(1);
+        }
+        cout << "<" <<st.top() << ">" << endl; 
         cin >> in;
     }
-
+    stackSize = st.size();
+    try{
+        if((stackSize - 1) > 1){
+            throw stackSize;
+        }
+    }
+    catch(int e){
+        cout << "ERROR: " << "There is more than one item on the stack when we want to print the result!" << endl;
+        exit(1);
+    }
     cout << "The answer is : " << st.top() << endl;
 
     return 0; 
